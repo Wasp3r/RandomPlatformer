1b9b4870507f3b97d30f51b8f0412082d2989410
Andrzej Osowski <andrzejosowski93@gmail.com>
2023-04-08 11:10:34 +0200

    Movement system finished

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,31 +4,50 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    /// <summary>
+    ///     The speed at which the player accelerates.
+    /// </summary>
     [SerializeField] 
     private float _accelerationSpeed = 1f;
 
+    /// <summary>
+    ///     The speed at which the player decelerates.
+    /// </summary>
     [SerializeField] 
     private float _decelerationSpeed = 2f;
     
+    /// <summary>
+    ///     The maximum speed at which the player can move.
+    /// </summary>
     [SerializeField] 
     private float _maxSpeed = 1f;
-    
-    public float _currentSpeed;
-    private Vector2 _previousPosition;
+
+    /// <summary>
+    ///     The rigidbody2D component of the player.
+    /// </summary>
+    [SerializeField] 
     private Rigidbody2D _rigidbody2D;
+    
+    /// <summary>
+    ///     The current speed of the player.
+    /// </summary>
+    private float _currentSpeed;
+    
+    /// <summary>
+    ///     The previous position of the player.
+    /// </summary>
+    private Vector2 _previousPosition;
 
-    void Start()
-    {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
+    /// <summary>
+    ///     We handle player movement in the Update method.
+    /// </summary>
+    void FixedUpdate()
     {
         var position = _rigidbody2D.position;
         var direction = position - _previousPosition;
-        _currentSpeed = Vector2.Distance(position, _previousPosition) / Time.deltaTime;
+        _currentSpeed = _rigidbody2D.velocity.magnitude;
         _previousPosition = position;
-
+        
         if (_currentSpeed >= _maxSpeed)
             return;
         
@@ -41,6 +60,7 @@ public class MovementController : MonoBehaviour
                 _rigidbody2D.AddForce(Vector2.left * _accelerationSpeed);
                 break;
             default:
+                direction.y = 0;
                 _rigidbody2D.AddForce(-direction * _decelerationSpeed);
                 break;
         }
