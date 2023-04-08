bab90b8ed6c93198111a1e774625058cd88edd2b
Andrzej Osowski <andrzejosowski93@gmail.com>
2023-04-08 12:21:02 +0200

    Jumping and moving system refactors WIP

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,61 +9,101 @@ public class MovementController : MonoBehaviour
     ///     The speed at which the player accelerates.
     /// </summary>
     [SerializeField] 
-    private float _accelerationSpeed = 1f;
+    private float _accelerationSpeed = 10f;
 
     /// <summary>
     ///     The speed at which the player decelerates.
     /// </summary>
     [SerializeField] 
-    private float _decelerationSpeed = 2f;
+    private float _decelerationSpeed = 300f;
     
     /// <summary>
     ///     The maximum speed at which the player can move.
     /// </summary>
     [SerializeField] 
-    private float _maxSpeed = 1f;
+    private float _maxSpeed = 10f;
+
+    /// <summary>
+    ///     The force at which the player jumps.
+    /// </summary>
+    [SerializeField] 
+    private float _jumpingForce = 30f;
 
     /// <summary>
     ///     The rigidbody2D component of the player.
     /// </summary>
     [SerializeField] 
     private Rigidbody2D _rigidbody2D;
+
+    [SerializeField] 
+    private LayerMask _jumpingLayerMask;
     
     /// <summary>
     ///     The current speed of the player.
     /// </summary>
     private float _currentSpeed;
-    
-    /// <summary>
-    ///     The previous position of the player.
-    /// </summary>
-    private Vector2 _previousPosition;
 
     /// <summary>
-    ///     We handle player movement in the Update method.
+    ///     The height of the player.
     /// </summary>
-    void FixedUpdate()
+    private float _playerHeight;
+
+    /// <summary>
+    ///     We get the player height in the Start method.
+    /// </summary>
+    private void Start()
     {
-        var position = _rigidbody2D.position;
-        var direction = position - _previousPosition;
+        _playerHeight = GetComponent<BoxCollider2D>().size.y;
+    }
+
+    /// <summary>
+    ///     We handle player movement and jumping
+    /// </summary>
+    private void Update()
+    {
         _currentSpeed = _rigidbody2D.velocity.magnitude;
-        _previousPosition = position;
+        transform.rotation = Quaternion.identity;
+        HorizontalMovement();
         
-        if (_currentSpeed >= _maxSpeed)
+        if (!ShouldJump())
             return;
         
+        Jump();
+    }
+
+    private void HorizontalMovement()
+    {
         switch (Input.GetAxisRaw("Horizontal"))
         {
             case 1:
-                _rigidbody2D.AddForce(Vector2.right * _accelerationSpeed);
+                Accelerate(Vector2.right);
                 break;
             case -1:
-                _rigidbody2D.AddForce(Vector2.left * _accelerationSpeed);
-                break;
-            default:
-                direction.y = 0;
-                _rigidbody2D.AddForce(-direction * _decelerationSpeed);
+                Accelerate(Vector2.left);
                 break;
         }
+    }
+    
+    private void Accelerate(Vector2 direction)
+    {
+        if (_currentSpeed >= _maxSpeed)
+            return;
+        
+        _rigidbody2D.AddForce(direction * _accelerationSpeed);
+    }
+
+    private bool ShouldJump() => Input.GetButtonDown("Jump") && CanJump();
+
+    private void Jump()
+    {
+        Debug.Log("### - Jumping");
+        _rigidbody2D.AddForce(Vector2.up * _jumpingForce, ForceMode2D.Impulse);
+    }
+
+    private bool CanJump()
+    {
+        var raycast = Physics2D.Raycast(_rigidbody2D.position, Vector2.down, _playerHeight + 0.01f, _jumpingLayerMask);
+        Debug.Log($"### - Collider: {raycast.collider}");
+        return raycast.collider != null;
     }
 }
