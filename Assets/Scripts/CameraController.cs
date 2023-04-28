ca4e92f5fe234a7a478a1ffccb7b454f81b7f193
Andrzej Osowski <andrzejosowski93@gmail.com>
2023-04-28 08:07:49 +0200

    Added camera smooth movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace RandomPlatformer
 {
@@ -16,7 +17,17 @@ namespace RandomPlatformer
         /// <summary>
         ///     Camera follow speed.
         /// </summary>
-        [SerializeField] private float _followSpeed = 5f;
+        [SerializeField] private float _maxFollowSpeed = 5f;
+
+        /// <summary>
+        /// Minimum camera Y position.
+        /// </summary>
+        [SerializeField] private float _minimumY;
+
+        /// <summary>
+        ///     Camera movement curve.
+        /// </summary>
+        [SerializeField] private AnimationCurve _movementCurve = AnimationCurve.Linear(0,0,1,1);
 
         /// <summary>
         ///     Camera transform reference.
@@ -46,7 +57,10 @@ namespace RandomPlatformer
             var targetPosition = _followTarget.position;
             var cameraPosition = _cameraTransform.position;
             targetPosition.z = cameraPosition.z;
-            _cameraTransform.position = Vector3.Lerp(cameraPosition, targetPosition, _followSpeed * Time.deltaTime);
+            targetPosition.y = Mathf.Max(targetPosition.y, _minimumY);
+            
+            var movementSpeed = GetMovementPosition(targetPosition, cameraPosition);
+            _cameraTransform.position = Vector3.Lerp(cameraPosition, targetPosition, movementSpeed);
         }
 
         /// <summary>
@@ -66,6 +80,19 @@ namespace RandomPlatformer
         {
             _isFollowing = false;
             _followTarget = null;
+        }
+
+        /// <summary>
+        ///     Get camera movement speed based on the distance between target and camera. We use animation curve to get the
+        ///     movement speed.
+        /// </summary>
+        /// <param name="targetPosition">Target position.</param>
+        /// <param name="cameraPosition">Camera position.</param>
+        /// <returns></returns>
+        private float GetMovementPosition(Vector3 targetPosition, Vector3 cameraPosition)
+        {
+            var distance = Vector2.Distance(targetPosition, cameraPosition);
+            return _movementCurve.Evaluate(distance) * _maxFollowSpeed * Time.deltaTime;
         }
     }
 }
