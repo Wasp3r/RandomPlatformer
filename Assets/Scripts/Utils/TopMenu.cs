938e02321e4bf7a17bc417f5e6fd49b84a3fea04
Andrzej Osowski <andrzejosowski93@gmail.com>
2023-05-12 09:03:45 +0200

    Level 3 WIP

---
+++ Assets/Scripts/Utils/TopMenu.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+namespace RandomPlatformer.Utils
+{
+#if UNITY_EDITOR
+    public class TopMenu
+    {
+        [MenuItem("RandomPlatformer/Play")]
+        public static void OpenMainScene()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/MainScene.unity");
+            EditorApplication.EnterPlaymode();
+        }
+    }
+#endif
+}
