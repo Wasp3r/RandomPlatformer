f3bde1c777a5de1c697f1b73d7c3bfd2c360f11d
AndrzejOsowskiU9 <135810063+AndrzejOsowskiU9@users.noreply.github.com>
2023-06-18 10:52:22 +0200

    Coin animations WIP

--- Assets/Scripts/ScoringSystem/Coin.cs
+++ Assets/Scripts/ScoringSystem/Coin.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace RandomPlatformer.ScoringSystem
 {
@@ -8,9 +10,26 @@ namespace RandomPlatformer.ScoringSystem
     public class Coin : MonoBehaviour
     {
         /// <summary>
+        ///     The animator of the coin.
+        ///     Used to put coin into floating and spinning animation.
+        /// </summary>
+        [SerializeField] private Animator _animator;
+        
+        /// <summary>
         ///     The number of points the coin is worth.
         /// </summary>
         [SerializeField] private int _points;
+
+        private static readonly int CycleOffset = Animator.StringToHash("CycleOffset");
+        private static readonly int SpinTrigger = Animator.StringToHash("Spin");
+        private static readonly int FloatTrigger = Animator.StringToHash("Float");
+
+        private void Start()
+        {
+            _animator.SetFloat(CycleOffset, Random.Range(0f,1f));
+            _animator.SetTrigger(SpinTrigger);
+            _animator.SetTrigger(FloatTrigger);
+        }
 
         /// <summary>
         ///     Picks up the coin and destroys it.
